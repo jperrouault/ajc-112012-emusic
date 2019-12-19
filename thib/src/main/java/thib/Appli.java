32f96886d1f5cef468384dd14b699159e9a80221
de la Chesnais <thibaut.delachesnais@gmail.com>
2019-12-19 14:51:19 +0100

    projet thib update

---
+++ thib/src/main/java/thib/Appli.java
@@ -0,0 +1,10 @@
+package thib;
+
+public class Appli {
+
+	public static void main(String[] args) {
+		System.out.println(" C'EST TOUT L'EFFET QUE CA TE FAIT QUAND JE TE DIS QU'ON VA MANGER DES SHIPS ???!!!");
+
+	}
+
+}
